0f03f7597bd82274c296c56c3fbc47e6f9a857ea
Elisha Atulomah <elishaubax@gmail.com>
2019-06-14 10:47:05 -0700

    Implemented lsls.c to read dir and print file name and size. Added gitignore to exclude .vscode folder.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 /**
  * Main
@@ -7,12 +9,34 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  int i;
+  struct stat buf;
+
+  printf("There are %d command line argument(s):\n", argc);
 
   // Open directory
+  for (i = 1; i < (argc); i++)
+  {
+    DIR *dir = opendir(argv[i]);
+    struct dirent *entry;
 
-  // Repeatly read and print entries
+    if (dir == NULL)
+    {
+      perror("opendir() error");
+    }
+    else
+    {
+      // Repeatly read and print entries
+      while ((entry = readdir(dir)) != NULL)
+      {
+        stat(entry->d_name, &buf);
 
-  // Close directory
+        printf("%10ld %s\n", buf.st_size, entry->d_name);
+      }
+      // Close directory
+      closedir(dir);
+    }
+  }
 
   return 0;
 }
